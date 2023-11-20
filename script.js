b96c900c9e2ca2b6f831eb1be4b16ada4e4f7ace
AlexOliveiraK <alex.mcaste@gmail.com>
2023-11-20 11:56:42 -0300

    adicao do detalhamento do apto

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function exibeApartamentos(){
             <p class="apto-nome">Nome:${element.nomeApto}</p>
             <div class="controles">
                 <button class="apagar" onclick="deleteApto(${index})">Deletar</button>
-                <button class="abrir-apto">Abrir</button>
+                <button class="abrir-apto" onclick="abreApto(${index})">Abrir</button>
             </div>
         </div>
         `
@@ -36,6 +36,8 @@ function exibeApartamentos(){
 
     let listaHtmlCompleta = document.querySelector('.apartamentos')
     listaHtmlCompleta.innerHTML = htmlApartamentos
+
+    document.querySelector('.aptoDetalhado').style.display = 'none'
 }
 
 function salvaLocalStorage(){
@@ -57,6 +59,36 @@ function deleteApto(id){
     apartamentos.splice(id, 1)
 
     salvaLocalStorage()
+    exibeApartamentos()
+}
+
+function abreApto(index){
+    document.querySelector('.apartamentos').style.display = 'none'
+    document.querySelector('.aptoDetalhado').style.display = 'block'
+
+    let htmlaptoDetalhado = document.querySelector('.aptoDetalhado')
+
+    console.log(htmlaptoDetalhado)
+
+    apto = apartamentos
+
+    htmlaptoDetalhado.innerHTML = 
+    `
+        <p>Numero do apartamento: ${apto[index].idApto}</p>
+        <p>Nome inquilino principal: ${apto[index].nomeApto}</p>
+        <p>Número de moradores: 2</p>
+        <p>Valor do aluguel: R$1000,00</p>
+        <p>Dia de vencimento: 7</p>
+        <p>Pagamento vencido: Não</p>
+        <p>Dias em atraso: 0</p>
+        <p>Tem pet: Sim</p>
+        <button class="voltar-tela-principal" onclick="voltarTelaPrincipal()">Voltar</button>
+    `
+}
+
+function voltarTelaPrincipal(){
+    document.querySelector('.aptoDetalhado').style.display = 'none'
+    document.querySelector('.apartamentos').style.display = 'grid'
     exibeApartamentos()
 }
 
